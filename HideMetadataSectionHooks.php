f4f9b5a163e3829a9e8e890714ce18b644e3f3ea
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2021-03-23 02:41:40 +0200

    Change the hook used, as the previous one was deprecated

--- HideMetadataSectionHooks.php
+++ HideMetadataSectionHooks.php
@@ -2,7 +2,7 @@
 
 class HideMetadataSectionHooks {
 	const METADATA_CONTENT = '<!-- Metadata -->';
-	
+
 	public static function onOutputPageParserOutput( OutputPage &$out, ParserOutput $parserOutput ) {
 		$parserOutput->mText = preg_replace(
 			'/\<li class="toclevel.*?#MetaData.*?\<\/li>\n/i', '', $parserOutput->mText
@@ -11,17 +11,21 @@ class HideMetadataSectionHooks {
 		return true;
 	}
 
-
-	public static function onParserSectionCreate( Parser $parser, $section, &$sectionContent, $showEditLinks ) {
+	/*
+	 * This hook is called at the beginning of Parser::internalParse().
+	 *
+	 * @param Parser $parser
+	 * @param string &$text Text to parse
+	 * @param StripState $stripState StripState instance being used
+	 */
+	public static function onParserBeforeInternalParse( $parser, &$text, $stripState ) {
 		if ( $parser->getTitle()->isSpecialPage() === true ) {
 			return true;
 		};
 
-		if ( stripos( $sectionContent, 'id="metadata"' ) !== false ) {
-			$sectionContent = self::METADATA_CONTENT;
-		}
-
-		return true;
+		//$text = "1ello! action is: $action";
+		$pattern = '(={1,6})\s*metadata\s*\1' ;
+		$text = preg_replace( "/$pattern/im", self::METADATA_CONTENT, $text );
 	}
 
 }
